716fbbccd32aad2a9c1e6b536b74d0071f5cd04b
Danila <sdanila512@gmail.com>
2020-10-23 00:14:13 +0700

    form ui

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,10 @@
+import '../css/style.css';
+import './plugins';
 import locations from './store/locations';
+
 
 locations.init().then(res => {
     console.log(res);
     console.log(locations);
-    console.log(locations.getCitiesByCountryCode("PE"));
+    
 });
---
+++ src/js/plugins/materialize.js
@@ -0,0 +1,34 @@
+import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/js/materialize.min.js';
+
+const select = document.querySelectorAll('select');
+M.FormSelect.init(select);
+
+
+export function getSelectInstance(elem) {
+    return M.FormSelect.getInstance(elem);
+}
+
+
+const autocomplete = document.querySelectorAll('.autocomplete');
+M.Autocomplete.init(autocomplete, {
+    data: {
+        Apple: null,
+        Mircosoft: null,
+        Google: 'https://placehold.it/250x250',
+    },
+});
+
+export function getAutocompleteInstance(elem) {
+    return M.Autocomplete.getInstance(elem);
+}
+
+
+const datepickers = document.querySelectorAll('.datepicker');
+M.Datepicker.init(datepickers, {
+    showClearBtn: true,
+});
+
+export function getDatepickerInstance(elem) {
+    return M.Datepicker.getInstance(elem);
+}
